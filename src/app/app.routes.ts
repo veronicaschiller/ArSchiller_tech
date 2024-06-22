240af3e243091ccd9bd5f8aacbd6d3413dd8bcbc
veronicaschiller <veronicaschiller17@gmail.com>
2024-06-22 20:22:27 -0300

    ✨ Create the login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ { path: 'login', component: LoginComponent }, // Adicione a rota para o login
+ // Outras rotas da sua aplicação
+];
+
+@NgModule({
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
+})
+export class AppRoutingModule { }
